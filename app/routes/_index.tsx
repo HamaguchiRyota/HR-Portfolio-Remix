ed7da04c07b82df25bb8ba18b2fc4de1eadf23a7
Jamix <52881627+HamaguchiRyota@users.noreply.github.com>
2024-06-17 09:35:02 +0900

    v1

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -9,40 +9,27 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div className="font-sans p-4">
-      <h1 className="text-3xl">Welcome to Remix</h1>
-      <ul className="list-disc mt-4 pl-6 space-y-2">
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/quickstart"
-            rel="noreferrer"
-          >
-            5m Quick Start
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/tutorial"
-            rel="noreferrer"
-          >
-            30m Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/docs"
-            rel="noreferrer"
-          >
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+    <div className="flex justify-center items-center h-screen w-screen">
+      <figure className="box-content h-62 w-62 bg-slate-100 rounded-xl p-8 dark:bg-slate-800">
+        <img className="w-24 h-24 rounded-full mx-auto" src="https://avatars.githubusercontent.com/u/52881627" alt="" width="384" height="512"/>
+        <div className="pt-6 text-center space-y-4">
+          <blockquote>
+            <p className="text-lg font-medium">
+              テスト<br></br>
+              テスト
+            </p>
+          </blockquote>
+          <figcaption className="font-medium">
+            <div className="text-sky-500 dark:text-sky-400">
+              Ryota Hamaguchi
+            </div>
+            <div className="text-slate-700 dark:text-slate-500">
+              Student
+            </div>
+          </figcaption>
+        </div>
+      </figure>
     </div>
+
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+    container: {
+      center: true,
+    },
+  },
+  plugins: [require("daisyui")],
+}
+
