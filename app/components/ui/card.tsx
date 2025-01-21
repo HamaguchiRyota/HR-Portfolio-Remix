61a4d179cfb897aa83e832e92c21b7f45d2d79ec
HamaguchiRyota <52881627+HamaguchiRyota@users.noreply.github.com>
2025-01-21 21:38:30 +0900

    記事書いた

--- app/components/ui/card.tsx
+++ app/components/ui/card.tsx
@@ -3,10 +3,11 @@ import React from "react";
 interface CardProps {
   title: string;
   description: string;
+  date: string;
   image: string;
 }
 
-const Card: React.FC<CardProps> = ({ title, description, image }) => {
+const Card: React.FC<CardProps> = ({ title, description, date, image }) => {
   return (
     <div className="rounded-2xl border border-indigo-500/100 border-color-indigo shadow-lg hover:shadow-indigo-500/50 duration-300 mb-auto">
       <img
@@ -21,6 +22,7 @@ const Card: React.FC<CardProps> = ({ title, description, image }) => {
       <div className="p-3 rounded-b-2xl bg-gray-900 shadow-inner">
         <h1 className="font-bold text-xl mb-3">{title}</h1>
         <p className="text-sm">{description}</p>
+        <p className="text-xs text-gray-400 mt-3">{date}</p>
       </div>
     </div>
   );
---
+++ app/routes/Article.001.tsx
@@ -0,0 +1,135 @@
+import type { MetaFunction } from "@remix-run/node";
+import NotFound from "~/img/notfound.png";
+import Calender from "~/components/svg/calender";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "Project - Frailty-Check-System" },
+    { name: "description", content: "Frailty-Check-System" },
+  ];
+};
+
+export default function Index() {
+  return (
+    <div className="selection:bg-indigo-300 selection:text-indigo-700">
+      <div className="p-4">
+        <div className="p-5">
+          <div className="w-full max-w-4xl justify-center mx-auto">
+            <img
+              className="rounded-2xl w-full"
+              src={NotFound}
+              alt="Project Thumbnail"
+              onContextMenu={(e) => e.preventDefault()}
+              onSelect={(e) => e.preventDefault()}
+              onMouseDown={(e) => e.preventDefault()}
+            />
+            <article className="mt-7 mb-7 pb-2 border-b-2 border-slate-500/15">
+              <h1 className="font-bold text-4xl">
+                ポートフォリオサイトを作成しました！🎉
+              </h1>
+
+              <div className="flex flex-row mt-1 gap-1 leading-7 text-slate-400/50">
+                <Calender />
+                <p className="">2025年1月21日</p>
+              </div>
+            </article>
+
+            <article className="">
+              <div className="border-l-4 border-indigo-500">
+                <h3 className="font-bold text-2xl mt-5 mb-5 ps-2">経緯</h3>
+              </div>
+              <p className="leading-8">
+                こんにちは！Hamaryoです！
+                <br />
+                今年で大学4年生になり、大学生生活も終盤に差し掛かりました。
+                <br />
+                情報学の学生なのに、ポートフォリオサイト持ってないじゃん！ということで、
+                今までの学習内容や制作物をまとめるためにポートフォリオサイトを作成しました。
+                <br />
+                過去に
+                <a
+                  class="underline decoration-indigo-500 font-bold"
+                  href="https://nextjs.org/"
+                  target="_blank"
+                >
+                  Next.js
+                </a>
+                の
+                <a
+                  class="underline decoration-indigo-500 font-bold"
+                  href="https://nextra.site/"
+                  target="_blank"
+                >
+                  Nextra
+                </a>
+                というライブラリを使用した作りかけがありましたが、
+                一部環境で勝手に組み文字になることがあり、めんどくさくなってやめました。
+                <br />
+                <a
+                  className="underline decoration-indigo-500 font-bold"
+                  href="https://hamaryo-portfolio.vercel.app/"
+                  target="_blank"
+                >
+                  ➡過去に作成していたポートフォリオなるもの...
+                </a>
+              </p>
+            </article>
+
+            <article className="">
+              <div className="border-l-4 border-indigo-500">
+                <h3 className="font-bold text-2xl mt-5 mb-5 ps-2">使用技術</h3>
+              </div>
+              <p className="leading-8">
+                今回のポートフォリオサイトはNext.jsではなく、
+                <a
+                  className="underline decoration-indigo-500 font-bold"
+                  href="https://remix.run/"
+                  target="_blank"
+                >
+                  Remix
+                </a>
+                というWebフレームワークを使用して作成しました。CSSフレームワークには
+                <a
+                  className="underline decoration-indigo-500 font-bold"
+                  href="https://tailwindcss.com/"
+                  target="_blank"
+                >
+                  TailwindCSS
+                </a>
+                と
+                <a
+                  className="underline decoration-indigo-500 font-bold"
+                  href="https://daisyui.com/"
+                  target="_blank"
+                >
+                  daisyUI
+                </a>
+                を使用しています。
+                <br />
+                Remixは「Outlet」という機能が特徴で、親ルート内に子ルートをレンダリングできるというコンポーネントがあります。
+                <br />
+                自分はそういうのは関係なく、去年のはじめ辺りからブイブイ言わせていることを知り、Remixを選択しました...
+                <br />
+              </p>
+            </article>
+
+            <article className="">
+              <div className="border-l-4 border-indigo-500">
+                <h3 className="font-bold text-2xl mt-5 mb-5 ps-2">
+                  今後の展望
+                </h3>
+              </div>
+              <p className="leading-8">
+                まだOutlet機能を使えていないので、今後プロジェクトの詳細ページに導入しようと思います。
+                <br />
+                その他にも、ダークモードやプロジェクト,記事の一覧ページ、タグ機能などを追加していきたいです。
+                <br />
+                乞うご期待！
+              </p>
+            </article>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
