c47a7025fa4a19cd5baffc118175e955ae5f944c
HamaguchiRyota <52881627+HamaguchiRyota@users.noreply.github.com>
2025-01-24 19:01:19 +0900

    jsonに記事内容を格納し、jsonからレンダリングする方法へ変更

--- app/routes/Article.001.tsx
+++ app/routes/Article.001.tsx
@@ -1,15 +1,16 @@
 import type { MetaFunction } from "@remix-run/node";
 import NotFound from "~/img/notfound.png";
 import Calender from "~/components/svg/calender";
+import articleData from "~/Article/hamaryo/001.json";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "Article - ポートフォリオサイトを作成しました！🎉" },
+    { title: `Article - ${articleData.title}` },
     { name: "description", content: "記事" },
   ];
 };
 
-export default function Index() {
+export default function Article() {
   return (
     <div className="selection:bg-indigo-300 selection:text-indigo-700">
       <div className="p-4">
@@ -25,107 +26,47 @@ export default function Index() {
             />
             <article className="mt-7 mb-7 pb-2 border-b-2 border-slate-500/15">
               <h1 className="font-bold sm:text-4xl sd:text-3xl text-2xl">
-                ポートフォリオサイトを作成しました！🎉
+                {articleData.title}
               </h1>
 
               <div className="flex flex-row mt-1 gap-1 leading-7 text-slate-400/50">
                 <Calender />
-                <p className="">2025年1月21日</p>
+                <p>{articleData.date}</p>
               </div>
             </article>
 
-            <article className="">
-              <div className="border-l-4 border-indigo-500">
-                <h3 className="font-bold text-2xl mt-5 mb-5 ps-2">経緯</h3>
-              </div>
-              <p className="leading-8">
-                こんにちは！Hamaryoです！
-                <br />
-                今年で大学4年生になり、大学生生活も終盤に差し掛かりました。
-                <br />
-                情報学の学生なのに、ポートフォリオサイト持ってないじゃん！ということで、
-                今までの学習内容や制作物をまとめるためにポートフォリオサイトを作成しました。
-                <br />
-                過去に
-                <a
-                  class="underline decoration-indigo-500 font-bold"
-                  href="https://nextjs.org/"
-                  target="_blank"
-                >
-                  Next.js
-                </a>
-                の
-                <a
-                  class="underline decoration-indigo-500 font-bold"
-                  href="https://nextra.site/"
-                  target="_blank"
-                >
-                  Nextra
-                </a>
-                というライブラリを使用した作りかけがありましたが、
-                一部環境で勝手に組み文字になることがあり、めんどくさくなってやめました。
-                <br />
-                <a
-                  className="underline decoration-indigo-500 font-bold"
-                  href="https://hamaryo-portfolio.vercel.app/"
-                  target="_blank"
-                >
-                  ➡過去に作成していたポートフォリオなるもの...
-                </a>
-              </p>
-            </article>
+            {Object.entries(articleData.content).map(
+              ([sectionTitle, sectionContent]) => (
+                <article key={sectionTitle}>
+                  <div className="border-l-4 border-indigo-500">
+                    <h3 className="font-bold text-2xl mt-5 mb-5 ps-2">
+                      {sectionTitle}
+                    </h3>
+                  </div>
+                  <p className="leading-8">{sectionContent}</p>
+                </article>
+              )
+            )}
 
-            <article className="">
-              <div className="border-l-4 border-indigo-500">
-                <h3 className="font-bold text-2xl mt-5 mb-5 ps-2">使用技術</h3>
-              </div>
-              <p className="leading-8">
-                今回のポートフォリオサイトはNext.jsではなく、
-                <a
-                  className="underline decoration-indigo-500 font-bold"
-                  href="https://remix.run/"
-                  target="_blank"
-                >
-                  Remix
-                </a>
-                というWebフレームワークを使用して作成しました。CSSフレームワークには
-                <a
-                  className="underline decoration-indigo-500 font-bold"
-                  href="https://tailwindcss.com/"
-                  target="_blank"
-                >
-                  TailwindCSS
-                </a>
-                と
-                <a
-                  className="underline decoration-indigo-500 font-bold"
-                  href="https://daisyui.com/"
-                  target="_blank"
-                >
-                  daisyUI
-                </a>
-                を使用しています。
-                <br />
-                Remixは「Outlet」という機能が特徴で、親ルート内に子ルートをレンダリングできるというコンポーネントがあります。
-                <br />
-                自分はそういうのは関係なく、去年のはじめ辺りからブイブイ言わせていることを知り、Remixを選択しました...
-                <br />
-              </p>
-            </article>
-
-            <article className="">
+            <article>
               <div className="border-l-4 border-indigo-500">
                 <h3 className="font-bold text-2xl mt-5 mb-5 ps-2">
-                  今後の展望
+                  参考リンク
                 </h3>
               </div>
-              <p className="leading-8">
-                まだOutlet機能を使えていないので、今後プロジェクトの詳細ページに導入しようと思います。
-                <br />
-                その他にも、ダークモードやプロジェクト,記事の一覧ページ、タグ機能などを追加していきたいです。
-                <br />
-                乞うご期待！
-              </p>
+              <ul className="leading-8">
+                {Object.entries(articleData.links).map(([text, url]) => (
+                  <li key={url}>
+                    <a
+                      className="underline decoration-indigo-500 font-bold"
+                      href={url}
+                      target="_blank"
+                    >
+                      {text}
+                    </a>
+                  </li>
+                ))}
+              </ul>
             </article>
           </div>
         </div>
