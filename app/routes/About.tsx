f008b10298722c4966af697740eaed18bfec3d3a
HamaguchiRyota <52881627+HamaguchiRyota@users.noreply.github.com>
2025-01-24 22:49:45 +0900

    空画像を修正と1:1の空画像を追加

--- app/routes/About.tsx
+++ app/routes/About.tsx
@@ -1,4 +1,5 @@
 import type { MetaFunction } from "@remix-run/node";
+import NotFound11 from "~/img/ntf1-1.png";
 
 export const meta: MetaFunction = () => {
   return [
@@ -17,6 +18,35 @@ export default function Article() {
               <h1 className="font-bold sm:text-4xl sd:text-3xl text-2xl pb-2">
                 このサイトについて
               </h1>
+            </article>
+
+            <article className="pt-5">
+              <div className="flex justify-center gap-10">
+                <img
+                  className="rounded-2xl w-1/4 skew-y-6"
+                  src={NotFound11}
+                  alt="Project Thumbnail"
+                  onContextMenu={(e) => e.preventDefault()}
+                  onSelect={(e) => e.preventDefault()}
+                  onMouseDown={(e) => e.preventDefault()}
+                />
+                <img
+                  className="rounded-2xl w-1/4 skew-y-6"
+                  src={NotFound11}
+                  alt="Project Thumbnail"
+                  onContextMenu={(e) => e.preventDefault()}
+                  onSelect={(e) => e.preventDefault()}
+                  onMouseDown={(e) => e.preventDefault()}
+                />
+                <img
+                  className="rounded-2xl w-1/4 skew-y-6"
+                  src={NotFound11}
+                  alt="Project Thumbnail"
+                  onContextMenu={(e) => e.preventDefault()}
+                  onSelect={(e) => e.preventDefault()}
+                  onMouseDown={(e) => e.preventDefault()}
+                />
+              </div>
             </article>
           </div>
         </div>
