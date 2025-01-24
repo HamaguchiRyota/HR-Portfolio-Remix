22afb8d73173f1d6a9b9e167a3b12a668edaba55
HamaguchiRyota <52881627+HamaguchiRyota@users.noreply.github.com>
2025-01-24 23:08:17 +0900

    logo

--- app/routes/About.tsx
+++ app/routes/About.tsx
@@ -1,5 +1,7 @@
 import type { MetaFunction } from "@remix-run/node";
 import NotFound11 from "~/img/ntf1-1.png";
+import Remix from "~/img/Remix.png";
+import Tailwindcss from "~/img/Tailwindcss.png";
 
 export const meta: MetaFunction = () => {
   return [
@@ -14,17 +16,17 @@ export default function Article() {
       <div className="p-4">
         <div className="p-5">
           <div className="w-full max-w-4xl justify-center mx-auto">
-            <article className="mt-7 mb-7 pb-2 border-b-2 border-slate-500/15">
+            <article className="mb-4 pb-2 border-b-2 border-slate-500/15">
               <h1 className="font-bold sm:text-4xl sd:text-3xl text-2xl pb-2">
-                このサイトについて
+                本サイトについて
               </h1>
             </article>
 
-            <article className="pt-5">
+            <article className="pt-5 pb-8 border-b-2 border-slate-500/15">
               <div className="flex justify-center gap-10">
                 <img
                   className="rounded-2xl w-1/4 skew-y-6"
-                  src={NotFound11}
+                  src={Remix}
                   alt="Project Thumbnail"
                   onContextMenu={(e) => e.preventDefault()}
                   onSelect={(e) => e.preventDefault()}
@@ -32,7 +34,7 @@ export default function Article() {
                 />
                 <img
                   className="rounded-2xl w-1/4 skew-y-6"
-                  src={NotFound11}
+                  src={Tailwindcss}
                   alt="Project Thumbnail"
                   onContextMenu={(e) => e.preventDefault()}
                   onSelect={(e) => e.preventDefault()}
@@ -47,6 +49,14 @@ export default function Article() {
                   onMouseDown={(e) => e.preventDefault()}
                 />
               </div>
+            </article>
+
+            <article className="pt-5">
+              <p className="text-lg leading-8">
+                本サイトは、Remixを利用して構築されたサイトです。Remixは、ReactをベースにしたWebアプリケーションフレームワークで、サーバーサイドレンダリングやプリレンダリングをサポートしています。
+                <br />
+                また、本サイトは、Tailwind CSSを利用してデザインされています。
+              </p>
             </article>
           </div>
         </div>
