2969336d42f19a94ef36dabbb5c6bcf601db2d15
HamaguchiRyota <52881627+HamaguchiRyota@users.noreply.github.com>
2025-01-24 17:09:06 +0900

    Speed Insights追加

--- app/root.tsx
+++ app/root.tsx
@@ -8,6 +8,7 @@ import {
 import type { LinksFunction } from "@remix-run/node";
 import "./tailwind.css";
 import Header from "~/components/ui/header";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const links: LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -34,6 +35,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <body>
         <Header />
         {children}
+        <SpeedInsights />
         <ScrollRestoration />
         <Scripts />
       </body>
